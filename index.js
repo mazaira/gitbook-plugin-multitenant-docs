32c1fdba500b1ed0523c8bff1e3101bd4d41e3ff
mazaira <mazaira@fidor.com>
2019-01-22 18:43:31 +0100

    no clue but latest state

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
   hooks: {
     "page:before": function(page) {
-      page.content = page.content.replace(/(<internal>)[\s\S]*(<\/internal>)/, '');
+      page.content = page.content.replace(/(\[internal\])[\s\S]*(\[\/internal\])/, '');
+      // page.content = page.content.replace(/(\[external\])|(\[\/external\])/,'');
       return page;
     }
   }
